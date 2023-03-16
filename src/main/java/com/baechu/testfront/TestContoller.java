367b9519d04f5a030c0529acd405dca8a8a56844
HBLEEEEE <lhbs0528@gmail.com>
2023-03-16 19:57:28 +0900

    Feat : 상세 페이지 프론트#3

--- src/main/java/com/baechu/testfront/TestContoller.java
+++ src/main/java/com/baechu/testfront/TestContoller.java
@@ -6,12 +6,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
