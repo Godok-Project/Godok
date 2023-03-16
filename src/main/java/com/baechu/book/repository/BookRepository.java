d98b20c9fc97e51aaa9499e181fd0f087a5e92c4
Han <email>
2023-03-16 22:47:08 +0900

    feat: 검색어 검색 기능 #9

--- src/main/java/com/baechu/book/repository/BookRepository.java
+++ src/main/java/com/baechu/book/repository/BookRepository.java
@@ -1,8 +1,15 @@
 package com.baechu.book.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.baechu.book.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+	@Query(value = "select * from book as b where b.title like concat('%',:query,'%') limit 10", nativeQuery = true)
+	List<Book> findBooksByWord(@Param("query") String query);
 }
