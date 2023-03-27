be70077fc2de91249c68996332b2684ff481537e
Han <email>
2023-03-27 09:52:08 +0900

    feat: 커서 페이지네이션 적용 #5 #9

--- src/main/java/com/baechu/book/controller/BookController.java
+++ src/main/java/com/baechu/book/controller/BookController.java
@@ -59,13 +59,13 @@ public class BookController {
 		@RequestParam(value = "publish", defaultValue = "") String publish,
 		@RequestParam(value = "author", defaultValue = "") String author,
 		@RequestParam(value = "totalRow", defaultValue = "10") Integer totalRow,
-		@RequestParam(value = "page", defaultValue = "0") Integer page,
 		@RequestParam(value = "category", defaultValue = "") String category,
-		@RequestParam(value = "babyCategory", defaultValue = "") String babyCategory
+		@RequestParam(value = "babyCategory", defaultValue = "") String babyCategory,
+		@RequestParam(value = "cursor", defaultValue = "1") Long cursor
 	) {
-		FilterDto filter = createDto(query, sort, year, star, minPrice, maxPrice, publish, author, page, totalRow,
-			category, babyCategory);
-		BookListDto result = bookService.searchByWord(filter);
+		FilterDto filter = createDto(query, sort, year, star, minPrice, maxPrice, publish, author, totalRow,
+			category, babyCategory, cursor);
+		BookListDto result = bookService.searchByCursor(filter);
 		model.addAttribute("result", result);
 		return "search";
 	}
@@ -80,8 +80,8 @@ public class BookController {
 	}
 
 	private FilterDto createDto(String query, Integer sort, Integer year, Integer star, Integer minPrice,
-		Integer maxPrice, String publish, String author, Integer page, Integer totalRow, String category,
-		String babyCategory) {
+		Integer maxPrice, String publish, String author, Integer totalRow, String category,
+		String babyCategory, Long cursor) {
 		// 나중에 RequestParam을 따로 받지말고 하나의 객체로 받도록 수정 필요.
 		return FilterDto.builder()
 			.query(query)
@@ -92,10 +92,10 @@ public class BookController {
 			.maxPrice(maxPrice)
 			.publish(publish)
 			.author(author)
-			.page(page)
 			.totalRow(totalRow)
 			.category(category)
 			.babyCategory(babyCategory)
+			.cursor(cursor)
 			.build();
 	}
 }
